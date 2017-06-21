ee1d0184d411f5848b277cd9ea2926dd7ac7f2f4
Imogen Hardy <imogen.hardy@gmail.com>
2017-06-21 20:20:55 +0100

    used .center to centre-align the text

--- directory.rb
+++ directory.rb
@@ -1,6 +1,6 @@
 def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
+  puts "Please enter the names of the students".center(75)
+  puts "To finish, just hit return twice".center(75)
   # Create an empty array
   students = []
   # Get the first name
@@ -9,7 +9,7 @@ def input_students
   while !name.empty? do
     # Add hash to the array for each student
     students << {name: name, cohort: :july}
-    puts "Now we have #{students.count} students"
+    puts "Now we have #{students.count} students".center(75)
     # Get another name
     name = gets.chomp
     end
@@ -18,28 +18,28 @@ def input_students
 end
 
 def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
+  puts "The students of Villains Academy".center(75)
+  puts "-------------".center(75)
 end
 
 # Print the list of students
 def print(students)
   count = 0
   while students.length > count
-    puts "#{count+1}, #{students[count][:name]} (#{students[count][:cohort]} cohort)"
+    puts "#{count+1}. #{students[count][:name]} (#{students[count][:cohort]} cohort)".center(75)
     count += 1
   end
 end
 
 # And finally the total number
 def print_footer(students)
-  puts "Overall, we have #{students.count} great students."
+  puts "Overall, we have #{students.count} great students.".center(75)
 end
 
 def select_by_letter(students, letter)
   students.each_with_index do |student, index|
     if student[:name][0] == letter
-      puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)"
+      puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)".center(75)
     end
   end
 end
@@ -47,7 +47,7 @@ end
 def select_by_length(students, length)
   students.each_with_index do |student, index|
     if student[:name].length < length
-      puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)"
+      puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)".center(75)
     end
   end
 end
