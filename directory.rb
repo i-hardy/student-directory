1b992e7019645c0024d3e133478405d3e7a27dad
Imogen Hardy <imogen.hardy@gmail.com>
2017-06-26 12:03:45 +0100

    refactored interactive menu into separate methods

--- directory.rb
+++ directory.rb
@@ -1,30 +1,40 @@
+@students = []
+
+def print_menu
+  puts "Please select from the following options:".center(75)
+  puts "1. Input students".center(75)
+  puts "2. Show the students".center(75)
+  puts "9. Exit".center(75)
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+    when "1"
+      # Input the students
+      students = input_students
+    when "2"
+      # Show the students
+      show_students
+    when "9"
+      # Terminate the program
+      exit
+    else
+      puts "I don't know what you meant, please try again\n"
+  end
+end
+
 def interactive_menu
-  students = []
   loop do
     # Print the menu and ask the user what to do
-    puts "Please select from the following options:"
-    puts "1. Input students"
-    puts "2. Show the students"
-    puts "9. Exit"
-    # Read the input and save it into a variable
-    selection = gets.chomp
+    print_menu
     # Do what the user has asked
-    case selection
-      when "1"
-        # Input the students
-        students = input_students
-      when "2"
-        # Show the students
-        print_header(students)
-        print(students)
-        print_footer(students)
-      when "9"
-        # Terminate the program
-        exit
-      else
-        "I don't know what you meant, please try again"
-    # Repeat
-    end
+    process(gets.chomp)
   end
 end
 
@@ -46,64 +56,63 @@ end
 def input_students
   puts "Please enter a student's name".center(75)
   puts "To finish, just hit return twice".center(75)
-  # Create an empty array
-  students = []
   # Call the student_info method to get initial input
   student_info
   # While loop to continue prompting for input
   while !@name.empty? do
     # Add hash to the array for each student
-    students << {name: @name, cohort: @cohort.downcase.to_sym, animal: @animal, height: @height}
-    plural = ("student" if students.count == 1 ) || ("students")
-    puts "Now we have #{students.count} #{plural}".center(75)
+    @students << {name: @name, cohort: @cohort.downcase.to_sym, animal: @animal, height: @height}
+    plural = ("student" if @students.count == 1 ) || ("students")
+    puts "Now we have #{@students.count} #{plural}".center(75)
     # Get another student
     student_info
   end
   #Return the array
-  students
+  @students
 end
 
-def print_header(students)
-  if students.count > 0
+def print_header
+  if @students.count > 0
     puts "The students of Villains Academy".center(75)
     puts "-------------".center(75)
   end
 end
 
 # Print the list of students
-def print(students)
+def print_students_list
   count = 0
-  while students.length > count
-    puts "#{count+1}. #{students[count][:name]} (#{students[count][:cohort]} cohort)".center(75)
+  while @students.length > count
+    puts "#{count+1}. #{@students[count][:name]} (#{@students[count][:cohort]} cohort)".center(75)
     count += 1
   end
 end
 
 # And finally the total number
-def print_footer(students)
-  if students.count > 0
-    puts "Overall, we have #{students.count} great students.".center(75)
+def print_footer
+  plural = ("student" if @students.count == 1 ) || ("students")
+  if @students.count > 0
+    puts "Overall, we have #{@students.count} great #{plural}.\n".center(75)
   end
 end
 
-def select_by_letter(students, letter)
-  students.each_with_index do |student, index|
-    if student[:name][0] == letter
-      puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)".center(75)
+def select_by_letter(letter)
+  @students.each_with_index do |student, index|
+    if @student[:name][0] == letter
+      puts "#{index+1}. #{@student[:name]} (#{@student[:cohort]} cohort)".center(75)
     end
   end
 end
 
-def select_by_length(students, length)
-  students.each_with_index do |student, index|
+def select_by_length(length)
+  @students.each_with_index do |student, index|
     if student[:name].length < length
       puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)".center(75)
     end
   end
 end
 
-def print_by_cohort(students)
-  sorted = students.sort_by { |student| student[:cohort] }
+def print_by_cohort
+  sorted = @students.sort_by { |student| student[:cohort] }
   sorted.each_with_index do |student, index|
     puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)".center(75)
   end
